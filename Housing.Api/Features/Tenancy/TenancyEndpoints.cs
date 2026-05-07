43024d7602267d5c27bcf7b35fccff41a0ec73a6
Carl hope <84421131+carlhope@users.noreply.github.com>
2026-05-07 15:37:11 +0100

    Map tenancy endpoints

---
+++ Housing.Api/Features/Tenancy/TenancyEndpoints.cs
@@ -0,0 +1,19 @@
+using Housing.Api.Features.Tenancy.Create;
+using Housing.Api.Features.Tenancy.Get;
+using Housing.Api.Features.Tenancy.List;
+using Housing.Api.Features.Tenancy.Terminate;
+
+namespace Housing.Api.Features.Tenancy;
+
+public static class TenancyEndpoints
+{
+    public static void MapTenancyEndpoints(this IEndpointRouteBuilder routes)
+    {
+        var group = routes.MapGroup("/tenancies");
+
+        group.MapListTenancies();
+        group.MapGetTenancy();
+        group.MapCreateTenancy();
+        group.MapTerminateTenancy();
+    }
+}
